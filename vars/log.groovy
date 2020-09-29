70d433e088201c0e8f8c00db9fa6ca4194345d2b
hadogit <hadogit@gmail.com>
2020-09-29 09:37:39 +0300

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,30 +1,37 @@
 vars/log.groovy
 
-def BLUE_COLOR = "\033[1m\033[34m"
-def YELLOW_COLOR  "\033[1m\033[33m"
-def RED_COLOR = "\033[1m\033[31m"
-def GREEN_COLOR = "\033[1m\033[32m"
+
+def loadColor(){  
+ BLUE_COLOR = '\033[1m\033[34m'
+ YELLOW_COLOR  '\033[1m\033[33m'
+ RED_COLOR = '\033[1m\033[31m'
+ GREEN_COLOR = '\033[1m\033[32m'
+}
 
 
 def info(message) {
+  loadColor()
   ansiColor('css') {
     echo BLUE_COLOR + "INFO: ${message}" + BLUE_COLOR
     }
 }
 
 def warning(message) {
+  loadColor()
   ansiColor('css') {
     echo YELLOW_COLOR + "WARNING: ${message}" + YELLOW_COLOR
     }
 }
 
 def error(message) {
+  loadColor()
   ansiColor('css') {
     echo RED_COLOR + "ERROR: ${message}" + RED_COLOR
     }
 }
 
 def success(message) {
+  loadColor()
   ansiColor('css') {
     echo GREEN_COLOR + "SUCCESS: ${message}" + GREEN_COLOR
     }
