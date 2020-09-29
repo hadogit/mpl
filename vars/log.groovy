5a3d42720657de65843ba4f0f09263f3592f9b9b
hadogit <hadogit@gmail.com>
2020-09-29 13:16:46 +0300

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -9,7 +9,7 @@ def loadColor(){
 def info(message) {
   loadColor()
   //ansiColor('css') {
- echo '\033[0;43m' + "[INFO]: ${message}" + '\033[0;43m'
+ echo '\033[34m' + "[INFO]: ${message}" + '\033[34m'
   //  }
 }
 
