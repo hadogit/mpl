57a858493fe5030dc5c7f4e40282d1cc393893b9
hadogit <hadogit@gmail.com>
2020-09-29 10:17:56 +0300

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,35 +1,35 @@
 def loadColor(){  
- BLUE_COLOR = '\\033[1m\033[34m'
- YELLOW_COLOR  '\\033[1m\033[33m'
- RED_COLOR = '\\033[1m\033[31m'
- GREEN_COLOR = '\\033[1m\033[32m'
+ BLUE_COLOR = '\033[0;34m'
+ YELLOW_COLOR  '\033[0;43m'
+ RED_COLOR = '\033[0;31m'
+ GREEN_COLOR = '\033[0;32m'
 }
 
 
 def info(message) {
   loadColor()
   //ansiColor('css') {
-    echo BLUE_COLOR + "INFO: ${message}" + BLUE_COLOR
+ echo "${BLUE_COLOR}" + "[INFO]: ${message}" + "${BLUE_COLOR}"
   //  }
 }
 
 def warning(message) {
   loadColor()
 //  ansiColor('css') {
-    echo YELLOW_COLOR + "WARNING: ${message}" + YELLOW_COLOR
+    echo "${YELLOW_COLOR}" + "[WARNING]: ${message}" + "${YELLOW_COLOR}"
  //   }
 }
 
 def error(message) {
   loadColor()
 //  ansiColor('css') {
-    echo RED_COLOR + "ERROR: ${message}" + RED_COLOR
+    echo "${RED_COLOR}" + "[ERROR]: ${message}" + "${RED_COLOR}"
 //    }
 }
 
 def success(message) {
   loadColor()
 //  ansiColor('css') {
-    echo GREEN_COLOR + "SUCCESS: ${message}" + GREEN_COLOR
+    echo "${GREEN_COLOR}" + "[SUCCESS]: ${message}" + "${GREEN_COLOR}"
 //    }
 }
