3da92ebf835602b338cbe657ccf706dec13f1b74
yakir.yaakobi <yakir.yaakobi@zerto.com>
2021-01-28 17:02:17 +0200

    adding config

--- resources/com/griddynamics/devops/mpl/modules/Checkout/GitCheckout.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Checkout/GitCheckout.groovy
@@ -3,8 +3,9 @@
  */
 stage("my Checkout"){
  //    steps{
-        git url: CFG.'git.url',
+    git url: CFG.'git.url',
     branch: CFG.'git.branch'
 //     }
+    CFG.'git.user' = "yakir"
 }
 //,    credentialsId: CFG.'git.credentials'
