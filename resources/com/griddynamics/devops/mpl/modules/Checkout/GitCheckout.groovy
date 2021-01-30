2db8c16fa3789cc9e2a3b2d1264da39dbef7d98c
yakir.yaakobi <yakir.yaakobi@zerto.com>
2021-01-30 11:29:09 +0200

    2

--- resources/com/griddynamics/devops/mpl/modules/Checkout/GitCheckout.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Checkout/GitCheckout.groovy
@@ -7,6 +7,8 @@ stage("my Checkout"){
     branch: CFG.'git.branch'
 //     }
     OUT.'git.user' = "yakir"
+    println OUT.'git.user'
+    out.'git.user' = "yakir"
     //OUT = CFG
 }
 //,    credentialsId: CFG.'git.credentials'
