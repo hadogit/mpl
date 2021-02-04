e905019d8aa8a1bef15f51d018aa294b2014c3c2
hadogit <hadogit@gmail.com>
2021-02-04 13:02:04 +0200

    Update Checkout.groovy

--- resources/com/griddynamics/devops/mpl/modules/Checkout/Checkout.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Checkout/Checkout.groovy
@@ -4,21 +4,21 @@
  
 import com.griddynamics.devops.mpl.Helper
 
-def Map out = [:]
 if( CFG.'git.url' )
-  temp = MPLModule('Git Checkout', CFG)
+  OUT = MPLModule('Git Checkout', CFG)
  // OUT = Helper.mergeMaps(CFG,MPLModule('Git Checkout', CFG))
 else
-  temp = MPLModule('Default Checkout', CFG)
+  OUT = MPLModule('Default Checkout', CFG)
  // OUT = Helper.mergeMaps(CFG,MPLModule('Default Checkout', CFG))
 
-println("temp: " + temp.getClass())
-println("temp.out: " + temp.'out'.getClass())
+//println("temp: " + temp.getClass())
+//println("temp.out: " + temp.'out'.getClass())
 println("CFG: " + CFG.getClass())
+println("OUT: " + OUT.getClass())
 println("---------------")
 
 println("CFG.git: " + CFG.'git')
 //OUT = Helper.mergeMaps(CFG,temp.'out')
 //log.info(out.'git.user')
-OUT = temp
+//OUT = temp
 log.info("OUT.out.git.user : " +  OUT.'out.git.user')
