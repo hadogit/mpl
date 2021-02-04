d2d43be47591141731e8eb473ea742b56b3f67fa
hadogit <hadogit@gmail.com>
2021-02-04 14:01:16 +0200

    Update Checkout.groovy

--- resources/com/griddynamics/devops/mpl/modules/Checkout/Checkout.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Checkout/Checkout.groovy
@@ -3,25 +3,29 @@
  */
  
 import com.griddynamics.devops.mpl.Helper
-
+def temp = [:]
 if( CFG.'git.url' )
   temp = MPLModule('Git Checkout', CFG)
  // OUT = Helper.mergeMaps(CFG,MPLModule('Git Checkout', CFG))
 else
   temp = MPLModule('Default Checkout', CFG)
  // OUT = Helper.mergeMaps(CFG,MPLModule('Default Checkout', CFG))
-
+OUT.'info.git.user' = temp.'info.git.user'
 //println("temp: " + temp.getClass())
 //println("temp.out: " + temp.'out'.getClass())
-println("CFG: " + CFG.getClass())
-println("OUT: " + temp.getClass())
-println("OUT.info: " + temp.'info'.getClass())
-println("---------------")
+//println("CFG: " + CFG.getClass())
+//println("OUT: " + temp.getClass())
+//println("OUT.info: " + temp.'info'.getClass())
+//println("---------------")
 
-println("CFG.git: " + CFG.'git')
-OUT.'output.git.user' = temp.'info.git.user'
+//println("CFG.git: " + CFG.'git')
+//OUT.'output.git.user' = temp.'info.git.user'
 //OUT = Helper.mergeMaps(CFG.'git',OUT.'info.git')
 //log.info(out.'git.user')
 //OUT = temp
-log.info("OUT.output.git.user : " +  OUT.'output.git.user')
-println(OUT.size())
+//log.info("OUT.output.git.user : " +  OUT.'output.git.user')
+//println(OUT.size())
+
+/**
+ * Common checkout module
+ */
